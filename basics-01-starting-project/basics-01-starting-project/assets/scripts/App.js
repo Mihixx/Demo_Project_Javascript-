3312c910e6357507493b113e88edb9020bfd8149
Mihixx <mihiranga@marketpushapps.com>
2024-09-07 01:41:38 +0530

    Add files via upload

---
+++ basics-01-starting-project/basics-01-starting-project/assets/scripts/App.js
@@ -0,0 +1,17 @@
+// alert('this Works');
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+
+function add(num1,num2){
+currentResult = currentResult +parseInt(userInput.value);
+outputResult(currentResult , '');
+}
+
+addBtn.addEventListener('click',add);
+
+
+
+
+
+
